a27967bfe5bb9389fa3fc51c077cd0d9b8297ded
Marc Portier <marc.portier@gmail.com>
2016-03-17 11:27:28 +0100

    toevoeging om beeldekes te checken

---
+++ imgs.js
@@ -0,0 +1,60 @@
+/*jslint node: true */
+/*jslint es5: true */
+/*jslint nomen: true */
+/*global setImmediate */
+
+"use strict";
+
+var rwcsv = require('./lib/rwcsv'),
+    fs = require('fs'),
+    path = require('path'),
+    argv = require('yargs'),
+    async = require('async'),
+    moment = require('moment'),
+    mc = require('./lib/mediacheck'),
+    settings;
+
+settings = argv
+    .usage('Checkt de opgelijste beelden in de verschillende xml files in de aangegeven windump-directory')
+    .example('$0  -o ./imgs-check.csv',
+             'Maakt de winid,status,media-uri dump in csvi formaat.')
+
+    .describe('output', 'folder waar de dump wordt geplaatst.')
+    .alias('o', 'output')
+    .default('o', path.join(__dirname, 'imgs-check.csv'))
+
+    .describe('input', 'folder waar de xml bestanden gelezen worden.')
+    .alias('i', 'input')
+    .default('i', path.join(__dirname, '_link-data/LOCAL/'))
+
+    .describe('recurse', 'folder waar de xml bestanden gelezen worden.')
+    .alias('r', 'recurse')
+    .default('r', false)
+
+    .argv;
+
+function writeCSV(outf, dbag, headers) {
+    var data = [];
+    Object.keys(dbag).forEach(function (k) {
+        data.push(dbag[k]);
+    });
+
+    rwcsv.write(outf, data, headers);
+}
+
+function imgCheck(s) {
+    var outd = path.join(s.input, "checks");
+    if (!fs.existsSync(outd)) { fs.mkdirSync(outd); }
+    mc.check(s.input, s.recurse, function (err, results) {
+        writeCSV(path.join(outd, "bad_images.csv"),   results.badimgs,
+                 ["winid", "deleted", "published", "imguri", "broken"]);
+        writeCSV(path.join(outd, "no_images.csv"),    results.noimgs,
+                 ["winid", "deleted", "published", "imguri"]);
+        writeCSV(path.join(outd, "error_images.csv"), results.errurls,
+                 ["winid", "deleted", "published", "imguri", "status"]);
+        writeCSV(path.join(outd, "multi_use.csv"),    results.multi_use,
+                 ["winid", "deleted", "published", "imguri"]);
+    });
+}
+
+imgCheck(settings);
